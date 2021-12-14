35ecf204ce7c40177cb57f902ba77a342639805c
williams <warriaga.191ci03@istta.edu.pe>
2021-12-14 00:13:36 -0500

    index3.0

--- index.php
+++ index.php
@@ -374,3 +374,77 @@ e</h4>
 
   <!-- visitors -->
 <!-- contact -->
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contáctenos
+</h4>
+				<p class="contact-agile2">Inscribíte a nuestros boletines
+</p>
+				<form  method="post" name="sentMessage" id="contactForm" >
+					<div class="control-group form-group">
+                        
+                            <label class="contact-p1">Nombre completo
+:</label>
+                            <input type="text" class="form-control" name="name" id="name" required >
+                            <p class="help-block"></p>
+                       
+                    </div>	
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Número de teléfono
+:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Dirección de correo electrónico:</label>
+                            <input type="email" class="form-control" name="email" id="email" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    
+                    
+                    <input type="submit" name="sub" value="Enviar" class="btn btn-primary">	
+				</form>
+				<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+					
+				}
+				?>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Conectate con nosotros
+</h4>
+			<p class="contact-agile1"><strong>Teléfono :</strong>+951 951-951951</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:tusolutionweb@gmail.com">LosLinces@gmail.com</a></p>
+			<p class="contact-agile1"><strong>Direccion :</strong> Cusco, Santiago</p>
+																
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								
+							</ul>
+			</div>
+			
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+<!-- /contact -->
