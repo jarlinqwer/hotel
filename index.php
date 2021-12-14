980815179784c87b4919aa218c2c52b002eb8e60
EduardoVc15 <evilela.191ci40@istta.edu.pe>
2021-12-13 23:19:34 -0500

    index 2.5

--- index.php
+++ index.php
@@ -216,4 +216,161 @@ include('db.php');
     </div>
 </div>
  	<!-- //about -->
-<!--sevices-->	
+<!--sevices-->
+<!--sevices-->
+<div class="advantages">
+	<div class="container">
+		<div class="advantages-main">
+				<h3 class="title-w3-agileits">Nuestros servicios
+	</h3>
+		   <div class="advantage-bottom">
+			 <div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
+			 	<div class="advantage-block ">
+					<i class="fa fa-credit-card" aria-hidden="true"></i>
+			 		<h4>Quédate primero, paga después! </h4>
+			 		<p>Sin embargo, en ciertas circunstancias y el deber o la obligación se producen con frecuencia que los placeres tienen a sus funciones,
+				.</p>
+									<p><i class="fa fa-check" aria-hidden="true"></i>Habitación decorada, con aire acondicionado
+				</p>
+									<p><i class="fa fa-check" aria-hidden="true"></i>Balcón privado
+				</p>
+			 		
+			 	</div>
+			 </div>
+			 <div class="col-md-6 advantage-grid right-w3ls wow zoomIn" data-wow-delay="0.3s">
+			 	<div class="advantage-block">
+					<i class="fa fa-clock-o" aria-hidden="true"></i>
+			 		<h4>Restaurante las 24 horas
+	</h4>
+						<p>Sin embargo, en ciertas circunstancias y el deber o la obligación se producen con frecuencia que los placeres tienen a sus funciones,
+	.</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>24 horas de servicio a la habitación
+	</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>Servicio de conserjería las 24 horas
+	</p>
+			 	</div>
+			 </div>
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--//sevices-->
+
+	 <!-- rooms & rates -->
+      <div class="plans-section" id="rooms">
+				 <div class="container">
+				 <h3 class="title-w3-agileits title-black-wthree">Habitaciones y tarifas
+</h3>
+						<div class="priceing-table-main">
+				 <div class="col-md-3 price-grid">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r1.jpg" alt=" " class="img-responsive" />
+							<h4>Habitación de lujo
+</h4>
+						</div>
+						<div class="price-gd-bottom">
+							   <div class="price-list">
+									<ul>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									
+								     </ul>
+							</div>
+							<div class="price-selet">	
+								<h3><span>S/.</span>320</h3>						
+								<a href="admin/reservation.php" >	Reservar ahora
+</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid ">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r2.jpg" alt=" " class="img-responsive" />
+							<h4>Habitación de lujo
+</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+									<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div>
+							<div class="price-selet">
+								<h3><span>S/.</span>220</h3>
+								<a href="admin/reservation.php" >Reservar ahora
+</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid lost">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r3.jpg" alt=" " class="img-responsive" />
+							<h4>Casa de huespedes
+e</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div>
+							<div class="price-selet">
+								<h3><span>S/.</span>180</h3>
+								<a href="admin/reservation.php" >Reservar ahora
+</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/r4.jpg" alt=" " class="img-responsive" />
+							<h4>Habitación individual
+</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div>
+							<div class="price-selet">
+								<h3><span>S/.</span> 150</h3>
+								<a href="admin/reservation.php" >Reservar ahora
+</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	 <!--// rooms & rates -->
+  <!-- visitors -->
+
+
+  <!-- visitors -->
+<!-- contact -->
