eab221da064bf6be23864ea9be159cbd52b8717c
EduardoVc15 <evilela.191ci40@istta.edu.pe>
2021-12-18 23:05:32 -0500

    0

---
+++ resources/views/PaginaPrincipal/PagPrincipal.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.welcome')
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>HLL Inicio</title>
+</head>
+<body>
+
+<p>aaaaaaaaaaaaaaaaa</p>
+</body>
+</html>
